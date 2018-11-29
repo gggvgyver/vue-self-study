757c7afadefe4feca053ea2bf38b07ddc8f756e3
gggvgyver <gggvgyver@gmail.com>
2018-11-29 21:59:03 +0900

    카운트연습

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,18 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+  <div>
+    <h1>{{title}}</h1>
+    <p>{{count}}</p>
+    <button @click="count++">추가</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
+  export default {
+    data () {
+      return {
+        title: "별이야 오늘도 내 무릎에서 자고 있구나!",
+        count: 1
+      }
     }
   }
-}
 </script>
