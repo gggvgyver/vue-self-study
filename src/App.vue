f27dc3cb39cf0f48dd3a2b70be79038d08173b80
gggvgyver <gggvgyver@gmail.com>
2018-11-29 22:12:14 +0900

    홈2추가

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,17 @@
     <h1>{{title}}</h1>
     <p>{{count}}</p>
     <button @click="count++">추가</button>
+    <Home2></Home2>
   </div>
 </template>
 
 <script>
+  import Home2 from './components/Home2'
+  
   export default {
+    components: {
+      Home2
+    },
     data () {
       return {
         title: "별이야 오늘도 내 무릎에서 자고 있구나!",
